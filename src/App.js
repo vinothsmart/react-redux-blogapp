e7dc1c61b5ec7ef354b88ed2178210d0865cb099
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-29 16:39:27 +0530

    Home Component Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.scss";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
