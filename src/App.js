59c9f68ea58009fb941fe2b2d547d167a552811d
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-30 17:38:20 +0530

    updated HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles/App.scss";
 import Home from "./components/pages/Home";
 import Navbar from "./components/elements/Navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import AddPost from "./components/Posts/AddPost";
