0c9912052678e3591f6d3186eab54590ce6822c7
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-30 16:08:10 +0530

    store created

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import "./styles/App.scss";
 import Home from "./components/pages/Home";
 import Navbar from "./components/elements/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
