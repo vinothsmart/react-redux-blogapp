c184c8cbc7d57dcd96922ebf45adf66062d91594
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-30 17:06:54 +0530

    post pages added

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import PostCard from "./PostCard";
+import { useSelector } from "react-redux";
+
+const Posts = () => {
+  const posts = useSelector((state) => state.post.posts);
+
+  return (
+    <div className="row">
+      {posts.map((postItem) => (
+        <PostCard postItem={postItem} />
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
